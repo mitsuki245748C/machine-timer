66fa0f0d3c23cc1ab061d70ef7032b9cba0e0bfb
ota hiraki <e245733@ie.u-ryukyu.ac.jp>
2026-09-15 15:19:14 +0900

    バナー追加、レイアウトを黒赤で表現

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="home">
+      <header className="banner">Machine Timer</header>
       <main className="machine-panel" aria-label="Machine Timer">
         <h1>Machine Timer</h1>
       </main>
