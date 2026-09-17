69ccde5caf7cc8ef6bae6aaa21f83b930c8e6fbc
Shun NAKAYAMA <e245749@ie.u-ryukyu.ac.jp>
2026-09-17 14:00:30 +0900

    feat: 予約機能作成

--- app/stopwatch/model.ts
+++ app/stopwatch/model.ts
@@ -1,8 +1,11 @@
-export type StopwatchAction = "start" | "stop" | "reset";
+export type StopwatchAction = "start" | "stop" | "reset" | "reserve";
 
 export type StopwatchState = {
   isRunning: boolean;
   seconds: number;
+  startedByMe: boolean;
+  isReserved: boolean;
+  reservedByMe: boolean;
 };
 
 export type StopwatchResponse = {
